93d608db36dce187e20eb766c43f38f950d7af19
Pratyaksh Rikhari <pratyaksh@plantd.life>
2024-11-07 16:24:57 +0530

    Navbar in big screens sorted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
-import "./globals.css";
-import  "bootstrap/dist/css/bootstrap.min.css"
-import Navbar from "@/components/Navbar";
+import './globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '@/components/Navbar';
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <script src="https://test.plantd.life/js/jquery.min.js"></script>
+                <script src="./app.js" />
+            </head>
+            <body>
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    );
 }
