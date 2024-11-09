9b7aff9aa53813d333bb0b9198cc460cba7b5b79
Ashish <ashish@plantd.life>
2024-11-09 10:44:33 +0530

    footer UI/responsiveness and font correction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import './globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '@/components/Navigation/Navbar/Navbar';
 import Footer from '@/components/Navigation/Footer/Footer';
+//👇 Import Open Sans font
+import { Nunito_Sans } from 'next/font/google'
+
+//👇 Configure our font object
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function RootLayout({
     children,
@@ -9,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={nunitoSans.className}>
             <head>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
             </head>
--- src/components/Navigation/Footer/Footer.tsx
+++ src/components/Navigation/Footer/Footer.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import './Footer.css';
+import { width } from '@fortawesome/free-solid-svg-icons/fa0';
 
 const Footer = () => {
     return (
@@ -14,27 +15,27 @@ const Footer = () => {
                         <p className="mt-lg-4 mt-md-2 mt-4 f-15 mb-2 foot-para">Plantd is just a seedling – young, fun and exciting. We’re capturing the best of both worlds in a movement to help our members GO green to MAKE green.</p>
                     </div>
                     <div className="col-md-4 contact-info">
-                        <div className="">
-                            <h4 className="f-18">Contact us</h4>
-                            <span className="f-15 fw-700">Email:</span>
+                        <div className="mt-4">
+                            <h4 className="f-18 mb-lg-4">Contact us</h4>
+                            <span className="f-15 fw-700">Email: </span>
                             <Link href="mailto:info@plantd.life" className="f-15 text-white">
                                 info@plantd.life
                             </Link>
-                            <h4 className="f-18 mt-lg-4 mt-md-2 mt-4">Download App</h4>
-                            <div className="foot-app-icon d-flex">
-                                <Link href="https://app.plantd.life/MW/Footer/AppDownload" target="_blank">
-                                    <img src="https://test.plantd.life/images/plantdimg/App-Store-White-1.png" alt="App Store" width={150} height={40} className="mt-3 me-md-2" />
+                            <h4 className="f-18 mt-lg-4 mb-lg-4 mt-4">Download App</h4>
+                            <div className="foot-app-icon d-flex mb-md-0 mb-4">
+                                <Link className='app-store' href="https://app.plantd.life/MW/Footer/AppDownload" target="_blank" style={{width: "30%"}}>
+                                    <img src="https://test.plantd.life/images/plantdimg/App-Store-White-1.png" alt="App Store" className="" />
                                 </Link>
-                                <Link href="https://app.plantd.life/MW/Footer/AppDownload" target="_blank">
-                                    <img src="https://test.plantd.life/images/plantdimg/Google-Play-White-1.png" alt="Google Play" width={150} height={40} className="mt-3" />
+                                <Link className='play-store' href="https://app.plantd.life/MW/Footer/AppDownload" target="_blank" style={{width: "30%"}}>
+                                    <img src="https://test.plantd.life/images/plantdimg/Google-Play-White-1.png" alt="Google Play" className="" />
                                 </Link>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <h4 className="f-18">Useful links</h4>
-                        <div className="d-flex">
-                            <ul className="list-unstyled footer-link mt-lg-3 mt-md-1 mt-3 me-md-5">
+                        <h4 className="f-18 mb-lg-4">Useful links</h4>
+                        <div className="useful-link-div d-flex justify-content-between">
+                            <ul className="list-unstyled footer-link ">
                                 <li>
                                     <Link href="/about" className="f-15 text-white">
                                         About us
@@ -57,7 +58,7 @@ const Footer = () => {
                                 </li>
                             </ul>
 
-                            <ul className="list-unstyled footer-link mt-lg-3 mt-md-1 mt-3 me-md-5">
+                            <ul className="list-unstyled footer-link ">
                                 <li>
                                     <Link href="/policies" className="text-white f-15">
                                         Policies
@@ -80,7 +81,7 @@ const Footer = () => {
                                 </li>
                             </ul>
                         </div>
-                        <div className="social-icons mt-3">
+                        <div className="social-icons mt-md-3 mb-md-0 mt-4 mb-2">
                             <Link href="https://facebook.com" className="text-white me-2">
                                 <i className="fa fa-facebook"></i>
                             </Link>
