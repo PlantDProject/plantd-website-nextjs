1e5975d362e5857463a0eb611af3f2318529aae5
Pratyaksh Rikhari <pratyaksh@plantd.life>
2024-11-14 11:43:28 +0530

    Env files added

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -4,7 +4,7 @@ import ProjectDetails from './ProjectDetails';
 export async function generateMetadata({ params }: any): Promise<Metadata> {
     const { slug } = await params;
 
-    const data = await fetch(`https://admin-dev.getplantd.com/configurations/get_project_data`);
+    const data = await fetch(`${process.env.API_URL}/configurations/get_project_data`);
     const response = await data.json();
     const e = response?.projectList?.items.find((proj: any) => proj.slug === slug);
 
@@ -28,7 +28,7 @@ export async function generateMetadata({ params }: any): Promise<Metadata> {
 export default async function Page({ params }: any) {
     const { slug } = await params;
 
-    const data = await fetch(`https://admin-dev.getplantd.com/configurations/get_project_data`);
+    const data = await fetch(`${process.env.API_URL}/configurations/get_project_data`);
     const response = await data.json();
     const projectsList = response?.projectList?.items || [];
 
