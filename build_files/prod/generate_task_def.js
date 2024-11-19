abed3e4c00b2d62b108d8e3f2746a588f1729bed
jishant-acharya <jishant@plantd.life>
2024-11-19 12:01:12 +0530

    Updated the Build files for Prod

---
+++ build_files/prod/generate_task_def.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+
+const [,, image, tag] = process.argv;
+
+const filePath = path.join(__dirname, 'task_def.json');
+let fileInformation = JSON.parse(fs.readFileSync(filePath).toString())
+
+fileInformation["containerDefinitions"][0]["image"] = `${image}:${tag}`
+
+fs.writeFileSync(filePath, JSON.stringify(fileInformation))
