2f557a74730cbfab8fbabf9af7efb9d52dc0904d
Pratyaksh Rikhari <pratyaksh@plantd.life>
2024-12-10 12:48:42 +0530

    Thankyou page added and images saved locally

--- src/app/giveaways/page.tsx
+++ src/app/giveaways/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
     const dataOngoing = await fetchGraphQL(GET_ONGOING_EVENTS);
     const dataPast = await fetchGraphQL(GET_PAST_EVENTS, { page: 1, size: 10 });
 
-    console.log("oNGOING", dataOngoing) 
+    console.log("oNGOING", JSON.stringify(dataOngoing))
 
     return <Giveaways onGoingEvents={dataOngoing?.data?.getOngoingEventsForWebsite?.events} completedEvents={dataPast?.data?.getPastEventsForWebsite?.events} />;
 }
